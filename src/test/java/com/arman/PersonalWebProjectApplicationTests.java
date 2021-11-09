328a7756520bc0a1fc5be6e3264980de2f03b546
Arman <life.is2short@outlook.com>
2021-11-09 22:44:14 +0800

    添加mysql、mybatis依赖，配置DB信息

--- src/test/java/com/arman/PersonalWebProjectApplicationTests.java
+++ src/test/java/com/arman/PersonalWebProjectApplicationTests.java
@@ -1,5 +1,6 @@
 package com.arman;
 
+import com.baomidou.mybatisplus.core.toolkit.AES;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,19 @@ class PersonalWebProjectApplicationTests {
 
     @Test
     void contextLoads() {
+        // // 生成 16 位随机 AES 密钥
+        String randomKey = "xxxxx";
+        System.out.println("randomKey = " + randomKey);
+
+        // 随机密钥加密
+        String url = AES.encrypt("xxxxxx", randomKey);
+        System.out.println("url = " + url);
+
+        String username = AES.encrypt("xxxxx", randomKey);
+        System.out.println("username = " + username);
+
+        String pwd = AES.encrypt("xxxxxx", randomKey);
+        System.out.println("pwd = " + pwd);
     }
 
 }
