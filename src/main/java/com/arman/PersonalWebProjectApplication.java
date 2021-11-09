3d95ebaaffe3fd63974a558ad3edd8ee8299133d
Arman <life.is2short@outlook.com>
2021-11-09 21:49:56 +0800

    init project

---
+++ src/main/java/com/arman/PersonalWebProjectApplication.java
@@ -0,0 +1,16 @@
+package com.arman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author arman
+ */
+@SpringBootApplication
+public class PersonalWebProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalWebProjectApplication.class, args);
+    }
+
+}
